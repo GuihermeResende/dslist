0e97a6a26c663e279b215f1308e2288a01e585f5
Guilherme Resende <guilherme5874@gmail.com>
2025-01-23 12:31:24 -0300

    get lists

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -13,40 +13,40 @@ import jakarta.persistence.Table;
 @Table(name = "tb_game")
 public class Game {
 	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
-	private String title;
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		private long id;
+		private String title;
+		
+		@Column(name = "game_year")
+		private Integer year;
+		private String genre;
+		private String platforms;
+		private Double score;
+		private String imgUrl;
+		
+		@Column(columnDefinition = "TEXT")
+		private String shortDescription;
+		
+		@Column(columnDefinition = "TEXT")
+		private String longDescription;
+		
+		public Game() {
+		}
 	
-	@Column(name = "game_year")
-	private Integer year;
-	private String genre;
-	private String platforms;
-	private Double score;
-	private String imgUrl;
-	
-	@Column(columnDefinition = "TEXT")
-	private String shortDescription;
-	
-	@Column(columnDefinition = "TEXT")
-	private String longDescription;
-	
-	public Game() {
-	}
-
-	public Game(long id, String title, Integer year, String genre, String platforms,  Double score, String imgUrl,
-			String shortDescription, String longDescription) {
-		super();
-		this.id = id;
-		this.title = title;
-		this.year = year;
-		this.genre = genre;
-		this.platforms = platforms;
-		this.score = score;
-		this.imgUrl = imgUrl;
-		this.shortDescription = shortDescription;
-		this.longDescription = longDescription;
-	}
+		public Game(long id, String title, Integer year, String genre, String platforms,  Double score, String imgUrl,
+				String shortDescription, String longDescription) {
+			super();
+			this.id = id;
+			this.title = title;
+			this.year = year;
+			this.genre = genre;
+			this.platforms = platforms;
+			this.score = score;
+			this.imgUrl = imgUrl;
+			this.shortDescription = shortDescription;
+			this.longDescription = longDescription;
+		}
 
 	public Double getScore() {
 		return score;
